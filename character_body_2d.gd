693f58eb77d6a266bd051f5664f0257911f7d44d
W1nso1 <abdiexl@gmail.com>
2025-06-23 12:59:49 -0600

    ALunesuno

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -5,9 +5,11 @@ var brinco = -400
 var gravedad = 1000
 @onready var sprite = $AnimatedSprite2D
 var ultima_direccion = 1  
+var vidas = 2
 
 func _ready():
 	add_to_group("jugador")
+	AnimVidas()
 	
 
 func _physics_process(delta):
@@ -37,6 +39,29 @@ func actualizar_animaciones(direccion):
 			sprite.play("derecha")
 	
 	sprite.flip_h = ultima_direccion < 0
+
+func ReducirVidas():
+	if vidas > 0:
+		vidas -= 1
+#	else:
+#		_on_reset_body_entered()
+	pass
+
+func AnimVidas():
+	if vidas >= 3:
+		$Node2D/vida_tres.visible = true
+		$Node2D/vida_dos.visible = true
+		$Node2D/vida_uno.visible = true
+	if vidas < 3:
+		$Node2D/vida_tres.visible = false
+		$Node2D/vida_dos.visible = true
+		$Node2D/vida_uno.visible = true
+	if vidas < 2:
+		$Node2D/vida_tres.visible = false
+		$Node2D/vida_dos.visible = false
+		$Node2D/vida_uno.visible = true
+	else:
+		pass
 
 func _on_reset_body_entered(body: Node2D) -> void:
 	get_tree().reload_current_scene()
