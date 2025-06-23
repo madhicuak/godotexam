08e861d765860e9e80b8bc800fa50b93f57616e0
W1nso1 <abdiexl@gmail.com>
2025-06-22 18:08:14 -0700

    Adomingotres

--- enemy.gd
+++ enemy.gd
@@ -14,7 +14,7 @@ func _player_process (delta):
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	is_following_player=true 
-	player = body
+#	player = body
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
